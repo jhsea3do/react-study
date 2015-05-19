90c4c9a09e6396c308fc2883c35a911196c38d22
jhsea3do <jhsea3do@ubunpara.(none)>
2015-05-20 00:59:42 +0800

    - add gulp support - init npm settings - update README.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # react-study
+
 React Study
+
+### Instructions
+
+```python
+
+npm install gulp bower react-tool -g
+npm install
+npm start
+
+```
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var 
+    fs   = require('fs'),
+  path   = require('path'),
+  gulp   = require('gulp'),
+  del    = require('del'),
+  gls    = require('gulp-live-server');
+
+gulp.task('clean', function() {
+  del(['build', 'dist'])
+});
+
+gulp.task('serve', function() {
+  var server = gls.static('.', 3000);
+  server.start();
+  gulp.watch(['public/**/*'], server.notify);
+});
+
+gulp.task('build', function() {
+  var dir = path.join(__dirname, 'build');
+  if(fs.existsSync(dir)) { return; }
+  fs.mkdirSync( dir );
+});
+
+gulp.task('dist', function() {
+  var dir = path.join(__dirname, 'dist');
+  if(fs.existsSync(dir)) { return; }
+  fs.mkdirSync( dir );
+});
+
+gulp.task('default', ['clean', 'build', 'dist']);
